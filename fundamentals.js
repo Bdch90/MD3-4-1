4922d8eafccd2ab2914b89569411787d0219de46
Your Name <you@example.com>
2019-06-03 10:41:45 +0700

    new

--- fundamentals.js
+++ fundamentals.js
@@ -9,8 +9,6 @@
 //   console.log(lang);
 //   console.log(target);
 // }
-
-
 // function main() {
 //     var x = 5;
 //     console.log(x);
@@ -22,8 +20,6 @@
 //     y = 100;
 //     console.log(y);
 // }
-
-
 // function main() {
 //   console.log(x);
 //   var x = 5;
@@ -31,7 +27,6 @@
 //     let y = 10;
 //     console.log(y);
 // }
-
 /**
  * Data types
  */
@@ -82,7 +77,6 @@
 //     } as IPost;
 //   }
 // }
-
 // function main() {
 //     let count = 50;
 //     if (count > 0) {
@@ -93,46 +87,37 @@
 //     console.log(count);
 //
 // }
-
-// function main() {
-//     console.log("for loop");
-//     let idx:number
-//     const keys = "abcdef";
-//     for (let idx = 0; idx < keys.length; ++idx) {
-//         console.log(keys[idx]);
-//     }
-//
-//     console.log("while loop");
-//     idx = 0;
-//     while (idx < keys.length) {
-//         console.log(keys[idx]);
-//         ++idx;
-//     }
-//
-//     console.log("do-while loop");
-//
-//     idx = 0;
-//     do {
-//         console.log(keys[idx]);
-//         ++idx;
-//     } while (idx < keys.length);
-//
-//     console.log("for-of loop");
-//     for (const item of keys) {
-//         console.log(item);
-//     }
-//
-//     console.log("for-in loop");
-//     const user = {
-//         name: 'Bob',
-//         age: 55
-//     };
-//
-//
-//     for (const key in user) {
-//         console.log(`${key}: ${user[key]}`);
-//     }
-// }
-
-
+function main() {
+    console.log("for loop");
+    var idx;
+    var keys = "abcdef";
+    for (var idx_1 = 0; idx_1 < keys.length; ++idx_1) {
+        console.log(keys[idx_1]);
+    }
+    console.log("while loop");
+    idx = 0;
+    while (idx < keys.length) {
+        console.log(keys[idx]);
+        ++idx;
+    }
+    console.log("do-while loop");
+    idx = 0;
+    do {
+        console.log(keys[idx]);
+        ++idx;
+    } while (idx < keys.length);
+    console.log("for-of loop");
+    for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
+        var item = keys_1[_i];
+        console.log(item);
+    }
+    console.log("for-in loop");
+    var user = {
+        name: 'Bob',
+        age: 55
+    };
+    for (var key in user) {
+        console.log(key + ": " + user[key]);
+    }
+}
 main();
